3ab06adb6736795c05d0aa12b85260aefb375802
David Audu <davidaudu1010@gmail.com>
2023-01-12 02:38:31 +0100

    "book"

--- books/models.py
+++ books/models.py
@@ -6,5 +6,5 @@ class Book(models.Model):
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
--- books/views.py
+++ books/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import *
+from .models import *
 
 # Create your views here.
+class BookListView(ListView):
+    template_name = "books/book_list.html"
+    model = Book
+    context_object_name = "book_list"
