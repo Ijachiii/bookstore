e90a558b19fe67c6ef7c7f59181bdc39a2efd14d
David Audu <davidaudu1010@gmail.com>
2023-01-12 02:55:45 +0100

    books

--- books/views.py
+++ books/views.py
@@ -7,3 +7,9 @@ class BookListView(ListView):
     template_name = "books/book_list.html"
     model = Book
     context_object_name = "book_list"
+
+
+class BookDetailView(DetailView):
+    template_name = "books/book_detail.html"
+    model = Book
+    context_object_name = "book"
