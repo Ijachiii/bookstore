214655216310d46ede36d62d3dd512e9aacae096
David Audu <davidaudu1010@gmail.com>
2023-01-16 13:09:43 +0100

    docker compose prod

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -27,7 +27,7 @@ env.read_env()
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DJANGO_DEBUG")
+DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
 ALLOWED_HOSTS = [".railway.app", "localhost", "127.0.0.1"]
 
